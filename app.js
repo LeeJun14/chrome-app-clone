cf9aa14de0bf0d8b650fae73f00d984d93d692cb
LeeJun14 <777ljy777@gmail.com>
2023-01-19 18:32:45 +0900

    chrome app clone 2.3

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+/* const는 상수 -> 변하지 않는다. */
 const a = 5;
 const b = 2;
-const name = "jun";
+/* let은 const와는 다르게 중간에 값을 변경가능 */
+let myName = "jun";
 
 console. log(a + b);
 console. log(a * b);
 console. log(a / b);
-console. log("hello " + name);
+console. log("hello " + myName);
+
+myName = "junyeon";
+
+console.log("your new name is " + myName);
