b83316c3b8a8c17e13e43174d8f9fb2086161fd4
LeeJun14 <777ljy777@gmail.com>
2023-01-25 18:35:09 +0900

    chrome app clone 3.1

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
 
+title.innerText = "Got you!";
 
-if(isNaN(age) || age < 0) {
-    console.log("Please write a real positive number");
-} else if(age < 19) {
-    console.log("You are too young.");
-} else if(age >= 19 && age <=50) {
-    console.log("You can drink");
-} else if(age > 50 && age <=80) {
-    console.log("You should exercise");
-} else if(age > 80) {
-    console.log("You can do whatever you want.");
-}
-
+console.log(title.id);
+console.log(title.className);
