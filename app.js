d3aed02a1a3fb58aebe1fc896181f248bb0ec007
LeeJun14 <777ljy777@gmail.com>
2023-01-25 14:20:15 +0900

    chrome app clone 2.13

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-const calculator = {
-    plus: function(a, b) {
-        return a + b;
-    },
-    minus: function(a, b) {
-        return a - b;
-    },
-    times: function(a, b) {
-        return a * b;
-    },
-    divide: function(a, b) {
-        return a / b;
-    },
-    power: function(a, b) {
-        return a ** b;
-    }
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(5, 5);
-const minusResult = calculator.minus(plusResult, 5);
-const timesResult = calculator.times(5, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
+
+console.log(age);
 
