06d3a0e0b207efb4af5f2ad2d6d95b2818ec70a0
LeeJun14 <777ljy777@gmail.com>
2023-01-26 10:57:46 +0900

    chrome app clone 3.6

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
 const h1 = document.querySelector("div.hello h1");
 
 function h1Click() {
-    h1.style.color = "blue";
+    const defaultColor = h1.style.color;
+    let newColor;
+    if (defaultColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue"
+    }
+    h1.style.color = newColor;
 }
 
-function h1MouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function h1MouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function windowResize() {
-    document.body.style.backgroundColor = "tomato"
-}
-
-function windowCopy() {
-    alert("copier!");
-}
-
-function windowOffline() {
-    alert("SOS no WIFI");
-}
-
-function windowOnline() {
-    alert("ALL GOOD!");
-}
 
 h1.addEventListener("click", h1Click);
-h1.addEventListener("mouseenter", h1MouseEnter);
-h1.addEventListener("mouseleave", h1MouseLeave);
-
-window.addEventListener("resize", windowResize);
-window.addEventListener("copy", windowCopy);
-window.addEventListener("offline", windowOffline);
-window.addEventListener("online", windowOnline);
-
-
-
