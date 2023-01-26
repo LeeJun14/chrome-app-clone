eebc5a1be70f94a2de3902dc3a0781aa2788b04d
LeeJun14 <777ljy777@gmail.com>
2023-01-26 10:32:06 +0900

    chrome app clone 3.4

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const title = document.querySelector("div.hello h1");
 function titleClick() {
     title.style.color = "blue";
 }
+function titleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+function titleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
 
 title.addEventListener("click", titleClick);
+title.addEventListener("mouseenter", titleMouseEnter);
+title.addEventListener("mouseleave", titleMouseLeave);
 
 
 
