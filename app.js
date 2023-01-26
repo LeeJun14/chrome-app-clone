9fb07e0c2c78f5cfa13828ffcfcb2a438e95e0a1
LeeJun14 <777ljy777@gmail.com>
2023-01-26 11:11:16 +0900

    chrome app clone 3.7

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector("div.hello h1");
 
 function h1Click() {
-    const defaultColor = h1.style.color;
-    let newColor;
-    if (defaultColor === "blue") {
-        newColor = "tomato";
+    const clickClass = "active";
+    if(h1.className === clickClass) {
+        h1.className = ""; 
     } else {
-        newColor = "blue"
+        h1.className = clickClass;
     }
-    h1.style.color = newColor;
 }
 
 
