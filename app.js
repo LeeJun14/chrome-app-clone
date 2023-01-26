d83643ab6d9d408feb2e5cb37a3a03494c4f5b89
LeeJun14 <777ljy777@gmail.com>
2023-01-26 10:47:17 +0900

    chrome app clone 3.5

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const title = document.querySelector("div.hello h1");
+const h1 = document.querySelector("div.hello h1");
 
-function titleClick() {
-    title.style.color = "blue";
-}
-function titleMouseEnter() {
-    title.innerText = "Mouse is here!";
-}
-function titleMouseLeave() {
-    title.innerText = "Mouse is gone!";
+function h1Click() {
+    h1.style.color = "blue";
 }
 
-title.addEventListener("click", titleClick);
-title.addEventListener("mouseenter", titleMouseEnter);
-title.addEventListener("mouseleave", titleMouseLeave);
+function h1MouseEnter() {
+    h1.innerText = "Mouse is here!";
+}
+
+function h1MouseLeave() {
+    h1.innerText = "Mouse is gone!";
+}
+
+function windowResize() {
+    document.body.style.backgroundColor = "tomato"
+}
+
+function windowCopy() {
+    alert("copier!");
+}
+
+function windowOffline() {
+    alert("SOS no WIFI");
+}
+
+function windowOnline() {
+    alert("ALL GOOD!");
+}
+
+h1.addEventListener("click", h1Click);
+h1.addEventListener("mouseenter", h1MouseEnter);
+h1.addEventListener("mouseleave", h1MouseLeave);
+
+window.addEventListener("resize", windowResize);
+window.addEventListener("copy", windowCopy);
+window.addEventListener("offline", windowOffline);
+window.addEventListener("online", windowOnline);
 
 
 
