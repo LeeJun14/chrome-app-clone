acbc36ad2cacce39345a04f690a1640e3c0c0cf6
LeeJun14 <777ljy777@gmail.com>
2023-01-26 11:17:11 +0900

    chrome app clone 3.8

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("div.hello h1");
 
 function h1Click() {
-    const clickClass = "active";
-    if(h1.className === clickClass) {
-        h1.className = ""; 
-    } else {
-        h1.className = clickClass;
-    }
+    h1.classList.toggle("active");
 }
 
 
